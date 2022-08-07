1cc8bef42f6b8e5fc527fc1db0d3c83dbbba9000
Furkan Acar <furkan@hotelrunner.com>
2022-08-08 00:09:41 +0300

    simplifies and explains the grep command

--- find-all-under-form.sh
+++ find-all-under-form.sh
@@ -1,8 +1,19 @@
-grep -Erho "t[(][\'\"]form[.]([[:alpha:]_]+).*[)]" ~/Workspace/lydia/src >> t_calls_with_form_keys.txt
+grep \
+-Erho \
+"t[(][\'\"]form[.]([[:alpha:]_]+).*[)]" \
+~/Workspace/lydia/src/pages/Reservations >> test.txt
 
+# Flags
 # -E	Extended expression (\escape meta-characters to use as literals)
 # -r	Search the given directory recursively
 # -h	Never print the filenames of the occurences
 # -o	Only print the matching part
 
-# >>	Output to a file
+# Pattern
+# Find all the t() function calls that takes any key under the form directory
+# and group the top-level key right after 'form'
+# e.g. 'form.account' -> account
+# e.g. 'form.foo.bar' -> foo
+
+# Target
+# Output to a file
