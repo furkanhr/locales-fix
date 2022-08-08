e51f8f8c221883ab513ec190ebb949a38fd9773a
Furkan Acar <furkan@hotelrunner.com>
2022-08-08 19:02:31 +0300

    updates pattern and find command to include missing matches of form keys

--- src/shell/find-form-keys.sh
+++ src/shell/find-form-keys.sh
@@ -1,7 +1,12 @@
+# Demo target
+# "$(pwd)/src/shell/example-target"
+
 grep \
 -Erho \
-"t[(][\'\"]form[.]([[:alpha:]_]+).*[)]" \
-~/Workspace/lydia/src >> t_calls_with_form_keys.txt
+"t[(][\'\"]form[.]([[:alpha:]_]+)" \
+~/Workspace/lydia/src \
+| sed "s/.*form[.]\(.*\).*/\1/g" \
+> "$(pwd)/src/shell/keysUnderForm.txt"
 
 # Flags
 # -E	Extended expression (\escape meta-characters to use as literals)
@@ -10,10 +15,19 @@ grep \
 # -o	Only print the matching part
 
 # Pattern
+# t[(][\'\"]form[.]([[:alpha:]_]+)
 # Find all the t() function calls that takes any key under the form directory
 # and group the top-level key right after 'form'
 # e.g. 'form.account' -> account
 # e.g. 'form.foo.bar' -> foo
+# e.g. 'form.foo_bar.baz' -> foo_bar
 
 # Target
+# ~/Workspace/lydia/src \
+
+# Replace
+# sed "s/.*form[.]\(.*\).*/\1/g" \
+# strip out anything but top-level key
+
 # Output to a file
+# > "$(pwd)/src/shell/keysUnderForm.txt"
