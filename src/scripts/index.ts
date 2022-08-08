3e17dbd7ce64a2b5a0f51b2a74b391f219a940c6
Furkan Acar <furkan@hotelrunner.com>
2022-08-08 22:59:06 +0300

    discards redundant script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,1 @@
 export * from "./extractPathsFromProd";
-export * from "./sort";
--- src/scripts/sort.ts
+++
@@ -1,9 +0,0 @@
-import { sortYamlFile } from "src/utils";
-
-const files = {
-  prodLocalesFilename: "prod.en.yml",
-  fixedLocalesFilename: "en.yml",
-  stagingLocalesFilename: "staging.en.yml",
-};
-
-Object.values(files).forEach(sortYamlFile);
