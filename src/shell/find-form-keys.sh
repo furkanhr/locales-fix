6f2edefd3150cafeddc35d942f3a922bd3ef3650
Furkan Acar <furkan@hotelrunner.com>
2022-08-08 23:15:32 +0300

    documents `find-form-keys.sh`

--- src/shell/find-form-keys.sh
+++ src/shell/find-form-keys.sh
@@ -9,13 +9,16 @@ grep \
 > "$(pwd)/form-keys.txt"
 
 # Flags
+#
 # -E	Extended expression (\escape meta-characters to use as literals)
 # -r	Search the given directory recursively
 # -h	Never print the filenames of the occurences
 # -o	Only print the matching part
 
 # Pattern
-# t[(][\'\"]form[.]([[:alpha:]_]+)
+#
+# "t[(][\'\"]form[.][[:alpha:]_]+"
+#
 # Find all the t() function calls that takes any key under the form directory
 # and group the top-level key right after 'form'
 # e.g. 'form.account' -> account
@@ -23,11 +26,14 @@ grep \
 # e.g. 'form.foo_bar.baz' -> foo_bar
 
 # Target
-# ~/Workspace/lydia/src \
+#
+# ~/Workspace/lydia/src
 
 # Replace
-# sed "s/.*form[.]\(.*\).*/\1/g" \
+#
+# sed "s/.*form[.]\(.*\)/\1/g"
 # strip out anything but top-level key
 
 # Output to a file
-# > "$(pwd)/src/shell/keysUnderForm.txt"
+#
+# > "$(pwd)/form-keys.txt"
