5d2fe096302d91551b98c72d3c52860a3dca002b
Furkan Acar <furkan@hotelrunner.com>
2022-08-09 10:59:40 +0300

    fixes target path for sort

--- src/utils/sortYamlFile.ts
+++ src/utils/sortYamlFile.ts
@@ -5,13 +5,13 @@ import yaml from "js-yaml";
 import { readFileSync } from "./readFileSync";
 
 export function sortYamlFile(filename: string) {
-  const file = readFileSync(
-    path.resolve(process.cwd(), "src", "yaml-files", filename)
-  );
+  const target = path.resolve(process.cwd(), "src", "yaml-files", filename);
+
+  const file = readFileSync(target);
 
   const data = yaml.load(file, { json: true });
 
   const sortedData = yaml.dump(data, { sortKeys: true, lineWidth: -1 });
 
-  fs.writeFileSync(path.resolve(filename), sortedData);
+  fs.writeFileSync(target, sortedData);
 }
