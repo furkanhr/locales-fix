8ee7a00d8a4339b122e4d2f63c218d49b3ede023
Furkan Acar <furkan@hotelrunner.com>
2022-08-09 10:38:54 +0300

    fixes relative path in `sort.ts`

--- src/scripts/sort.ts
+++ src/scripts/sort.ts
@@ -1,4 +1,4 @@
-import { sortYamlFile } from "src/utils";
+import { sortYamlFile } from "../utils";
 
 // ? The first two args are paths of node and this file respectively.
 const [filename] = process.argv.slice(2);
